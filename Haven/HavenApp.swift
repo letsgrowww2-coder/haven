90bea0c25e30ada48b9575f3361d03a950a83cee
Reyaansh Bansal <reyaansh.bansal@gmail.com>
2026-06-13 13:10:47 -0700

    Initial Commit

---
+++ Haven/HavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HavenApp.swift
+//  Haven
+//
+//  Created by Reyaansh Bansal on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct HavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HavenTests/HavenTests.swift
@@ -0,0 +1,18 @@
+//
+//  HavenTests.swift
+//  HavenTests
+//
+//  Created by Reyaansh Bansal on 6/13/26.
+//
+
+import Testing
+
+struct HavenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
